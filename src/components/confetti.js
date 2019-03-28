be25a7d1ae1f80fdc9b927e7301a59d913ec1b55
alanlanglois <alan@etaminstudio.com>
2019-03-28 16:02:43 +0100

    adding custom colors, small perf refacto and fadeout

--- src/components/confetti.js
+++ src/components/confetti.js
@@ -8,34 +8,24 @@ import {randomValue} from '../utils';
 type Props = {|
   left: Animated.Interpolation,
   bottom: Animated.Interpolation,
-  transform: Array<{ [key: string]: Animated.Interpolation }>
+  transform: Array<{ [key: string]: Animated.Interpolation }>,
+  color:string,
+  opacity: Animated.Interpolation,
 |};
 
-const colors: Array<string> = [
-  '#e67e22',
-  '#2ecc71',
-  '#3498db',
-  '#84AAC2',
-  '#E6D68D',
-  '#F67933',
-  '#42A858',
-  '#4F50A2',
-  '#A86BB7',
-  '#e74c3c',
-  '#1abc9c'
-];
+
 
 class Confetti extends React.PureComponent<Props> {
   props: Props;
   width: number = randomValue(8, 16);
   height: number = randomValue(6, 12);
   isRounded: boolean = Math.round(randomValue(0, 1)) === 1;
-  backgroundColor: string = colors[Math.round(randomValue(0, colors.length - 1))];
+  backgroundColor: string = this.props.color;
 
   render() {
-    const { left, bottom, transform } = this.props;
+    const { left, bottom, transform, opacity} = this.props;
     const { width, height, isRounded, backgroundColor } = this;
-    const style = { left, bottom, width, height, backgroundColor, transform };
+    const style = { left, bottom, width, height, backgroundColor, transform, opacity};
 
     return (
       <Animated.View style={[styles.confetti, isRounded && styles.rounded, style]} />
