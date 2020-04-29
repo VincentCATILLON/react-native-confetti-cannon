e507a8cac954fcc6fe1408086d6c35b20b16d2d2
Vincent Catillon <contact@vincent-catillon.fr>
2020-04-29 23:02:49 +0200

    Use native driver to enhance performances

--- src/components/confetti.js
+++ src/components/confetti.js
@@ -3,17 +3,14 @@
 import * as React from 'react';
 import { StyleSheet, Animated } from 'react-native';
 
-import {randomValue} from '../utils';
+import { randomValue } from '../utils';
 
 type Props = {|
-  left: Animated.Interpolation,
-  bottom: Animated.Interpolation,
+  containerTransform: Array<{ [key: string]: Animated.Interpolation }>,
   transform: Array<{ [key: string]: Animated.Interpolation }>,
   color: string,
   opacity: Animated.Interpolation,
 |};
-
-
 
 class Confetti extends React.PureComponent<Props> {
   props: Props;
@@ -23,19 +20,24 @@ class Confetti extends React.PureComponent<Props> {
   backgroundColor: string = this.props.color;
 
   render() {
-    const { left, bottom, transform, opacity } = this.props;
+    const { containerTransform, transform, opacity } = this.props;
     const { width, height, isRounded, backgroundColor } = this;
-    const style = { left, bottom, width, height, backgroundColor, transform, opacity};
+    const containerStyle = { transform: containerTransform };
+    const style = { width, height, backgroundColor, transform, opacity};
 
     return (
-      <Animated.View style={[styles.confetti, isRounded && styles.rounded, style]} />
+      <Animated.View style={[styles.confetti, containerStyle]}>
+        <Animated.View style={[isRounded && styles.rounded, style]} />
+      </Animated.View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   confetti: {
-    position: 'absolute'
+    position: 'absolute',
+    left: 0,
+    bottom: 0
   },
   rounded: {
     borderRadius: 100
